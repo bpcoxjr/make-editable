5a5a703a9af9177d09d514ea422d6232be5d05ff
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-03-31 19:29:36 -0400

    minor style changes

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 angular.module('makeEditableApp', [])
 .directive('makeEditable', function() {
 	return {
+		//restrict set to 'A' because we are creating a directive that modifies an element's attribute
 		restrict: 'A',
 		transclude: true,
 		templateUrl: 'make-editable.html',
+		//setting scope to true ensures a new, separate scope for each instantiation of the directive
 		scope: true,
 	}
 });
